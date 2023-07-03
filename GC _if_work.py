a2eb05fee6f34adfc65957b82e1e2c932a7b656e
CS01-Jiraphat <jiraphat20.jansook@gmail.com>
2023-07-03 21:05:59 +0700

    Update GC _if_work.py

--- GC _if_work.py
+++ GC _if_work.py
@@ -4,6 +4,7 @@ if age >= 18:
     print("You can entry.")
 else:
     print("You can not entry.")
+print("________________________________________")
 
 #2
 x = int(input("Number: "))
@@ -13,6 +14,7 @@ elif x > 0:
     print("Positive number")
 elif x == 0:
     print("Zero number")
+print("________________________________________")
 
 #3
 x = int(input("Enter age: "))
@@ -20,3 +22,22 @@ if 18 <= x <= 25:
     print("อายุ x อยู่ในช่วง 18-25")
 else:
     print("อายุ x ไม่อยู่ในช่วง 18-25")
+print("________________________________________")
+
+#4
+x = int(input("Number: "))
+if x < 10:
+    print("เป็นเลขหลักเดียว")
+elif x >= 10:
+    print("เป็นเลขสองหลัก")
+print("________________________________________")
+
+#5
+x = int(input("Number 1: "))
+y = int(input("Number 2: "))
+z = int(input("Number 3: "))
+if x == y == z:
+    print("x, y และ z เท่ากัน")
+else:
+    print("x, y และ z ไม่เท่ากัน")
+print("________________________________________")
