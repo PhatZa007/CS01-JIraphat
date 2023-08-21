0c334ded2a57dcaf06840fdb704fe15319f9e863
CS01-Jiraphat <jiraphat20.jansook@gmail.com>
2023-08-21 18:23:15 +0700

    while loop.py

---
+++ while loop.py
@@ -0,0 +1,43 @@
+num = 1
+while(num <= 20):
+    print(num, end = " ")
+    num = num + 1
+
+print(" ")
+
+while True:
+    user_input = int(input("Enter an integer: "))
+    if user_input % 2 != 0:
+        print("This number is odd")
+        break
+    print("This number is even")
+
+num  = int(input("Number: ")) ; out = 0
+while(out < num):
+    print("Hello") ; out = out + 1
+
+#While loop work.
+#1
+while True:
+    num = int(input("Number: "))
+    if num == 0:
+        print(num)
+        break
+    print(num)
+
+#2
+
+
+#3
+b = 0
+while(num != 0):
+    num = int(input("Number: "))
+    print(b + 1)
+
+
+
+
+
+
+    
+     
